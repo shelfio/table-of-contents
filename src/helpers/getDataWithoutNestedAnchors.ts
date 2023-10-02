fcaaecb453bfefcf997b74494e52ff566c78abf8
Mikolaj Duchewicz <mikolaj.duchewicz@shelf.io>
2023-10-02 09:42:39 +0200

    feat: Remove nested anchor tags from header

---
+++ src/helpers/getDataWithoutNestedAnchors.ts
@@ -0,0 +1,42 @@
+import type {TemplateExecutor} from 'lodash';
+import type {Header} from '../types';
+
+const parser = new DOMParser();
+
+const hasAnchorTagsInHeader = (data: Header) => {
+  const doc = parser.parseFromString(data.header, 'text/xml');
+
+  // Extract any existing anchor tags from the header variable
+  const anchorElement = doc.querySelector('a');
+
+  return anchorElement?.outerHTML;
+};
+
+export const getDataWithoutNestedAnchors = (data: Header, headerTemplate: TemplateExecutor) => {
+  const results = headerTemplate(data);
+
+  if (!hasAnchorTagsInHeader(data)) {
+    return headerTemplate(data);
+  }
+
+  const doc = parser.parseFromString(results, 'text/xml');
+
+  // get Toc pointer anchor node
+  const firstAnchorElement = doc.querySelector(`a[href="#${data.anchor}"]`);
+
+  // get all content nodes from Toc pointer anchor node
+  const contentArray = Array.from(firstAnchorElement?.childNodes ?? []);
+
+  const parentNode = firstAnchorElement?.parentElement;
+
+  // remove all content nodes from Toc pointer anchor node
+  contentArray.forEach(node => {
+    firstAnchorElement?.removeChild(node);
+  });
+
+  const newContentArray = [firstAnchorElement, ...contentArray] as Node[];
+
+  parentNode?.replaceChildren(...newContentArray);
+
+  return parentNode?.outerHTML || results;
+};
