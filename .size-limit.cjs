6c41895a717e8b4b14e9b880c78b658c3164d914
Yaroslav Chapelskyi <chapelskyi.slavik@gmail.com>
2023-10-30 22:06:09 +0200

    build: introduce bundlesize

---
+++ .size-limit.cjs
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    name: 'main',
+    path: 'lib/index.js',
+    limit: '39 KB',
+  },
+  {
+    name: 'main',
+    path: 'lib/index.js',
+    import: '{DEFAULT_SETTINGS}',
+    limit: '39 KB',
+  },
+];
