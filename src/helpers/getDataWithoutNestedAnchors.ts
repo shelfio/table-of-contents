4672893fd8516dc87e579401677e4ac3b67be7bb
hmelenok <hmelenok@gmail.com>
2023-11-07 13:28:46 +0200

    feat: GEMPAGEAPP-1566 Remove empty anchors

--- src/helpers/getDataWithoutNestedAnchors.ts
+++ src/helpers/getDataWithoutNestedAnchors.ts
@@ -4,7 +4,7 @@ import type {Header} from '../types.js';
 const parser = new DOMParser();
 
 const hasAnchorTagsInHeader = (data: Header) => {
-  const doc = parser.parseFromString(data.header, 'text/xml');
+  const doc = parser.parseFromString(data.all, 'text/html');
 
   // Extract any existing anchor tags from the header variable
   const anchorElement = doc.querySelector('a');
@@ -22,12 +22,20 @@ export const getDataWithoutNestedAnchors = (data: Header, headerTemplate: Templa
   const doc = parser.parseFromString(results, 'text/xml');
 
   // get Toc pointer anchor node
-  const firstAnchorElement = doc.querySelector(`a[href="#${data.anchor}"]:not(:empty)`);
-
-  // get all content nodes from Toc pointer anchor node
-  const contentArray = Array.from(firstAnchorElement?.childNodes ?? []);
+  const firstAnchorElement = doc.querySelector(`a[href="#${data.anchor}"]`);
 
   const parentNode = firstAnchorElement?.parentElement;
+  // EDGECASE - other anchors in this header
+  const otherAnchorElements = doc?.querySelectorAll(`a:not([href="#${data.anchor}"]):empty`);
+
+  if (otherAnchorElements && otherAnchorElements?.length > 0) {
+    otherAnchorElements.forEach(node => {
+      parentNode?.removeChild(node);
+    });
+  }
+  // get all content nodes from Toc pointer anchor node
+
+  const contentArray = Array.from(firstAnchorElement?.childNodes ?? []);
 
   // remove all content nodes from Toc pointer anchor node
   contentArray.forEach(node => {
